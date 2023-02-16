0d6d656f5ffc7aca354e07cded31367d690a4a6c
dinglin <linqian126@126.com>
2023-02-17 00:25:23 +0800

    add logo

--- src/views/login/signIn.vue
+++ src/views/login/signIn.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="login-page" v-loading="loading" element-loading-text="正在登录">
-    <div class="login-page-logo"></div>
+  <div class="login-page">
+    <div class="login-page-logo">
+      <img src="/public/vite.svg" height="200" />
+    </div>
     <div class="login-page-form">
       <el-input
         class="user-inp"
@@ -18,8 +20,13 @@
         placeholder="请输入密码"
       ></el-input>
       <el-checkbox v-model="form.checked" label="记住密码" size="large" />
-      <el-button size="large" @click="submit" type="success" class="submit"
-        >登陆</el-button
+      <el-button
+        :disabled="loading"
+        size="large"
+        @click="submit"
+        type="success"
+        class="submit"
+        >登陆{{ loading ? '中' : '' }}</el-button
       >
     </div>
   </div>
@@ -46,13 +53,20 @@ const submit = () => {
   box-sizing: border-box;
   width: 100vw;
   height: 100vh;
+  background: cornflowerblue;
   &-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 300px;
   }
   &-form {
     margin: auto;
+    padding: 20px;
+    border-radius: 5px;
     width: 300px;
+    background: #fff;
     .user-inp {
       margin-bottom: 20px;
     }
